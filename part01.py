c7b3c5c9ad0dd368b008dc9eef9e07d9774fd543
unknown <usmancali53@gmail.com>
2024-10-10 23:08:26 +0300

    while

--- part01.py
+++ part01.py
@@ -5,6 +5,16 @@ print(prifix[0])
 print(prifix[-1])
 s='mynameisusmanalihassenandamfromdiredawa'
 print(len(s))
+#list 
 list=[1,2,4,6,8]
 list=list + [10,12,14,16]
 print(list)
+latters=['a','b','c','d','e','f','g','h']
+latters[2:5]=['C','D','E']
+print(latters)
+latters[2:5]=[]
+print(latters)
+a, b = 0, 1
+while a<10:
+    print(a)
+    a, b = b, b+a
