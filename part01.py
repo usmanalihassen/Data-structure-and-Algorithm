9e2225cda1909fa13d53c9d203d9fa0968b9776f
unknown <usmancali53@gmail.com>
2024-10-10 21:31:53 +0300

    first commit

---
+++ part01.py
@@ -0,0 +1,10 @@
+prifix='py'
+prifix + 'thon'
+print(prifix+'thon')
+print(prifix[0])
+print(prifix[-1])
+s='mynameisusmanalihassenandamfromdiredawa'
+print(len(s))
+list=[1,2,4,6,8]
+list=list + [10,12,14,16]
+print(list)
