0ceccd5786eee25e6b99168ea3c3923bdaa55ff5
unknown <usmancali53@gmail.com>
2024-10-14 21:28:01 +0300

    argument

---
+++ arguments.py
@@ -0,0 +1,10 @@
+def calculate_area(length, width):
+    area = length * width
+    return area
+
+# Example usage
+length = 5
+width = 3
+result = calculate_area(length, width)
+
+print(f"The area of the rectangle is {result}")
