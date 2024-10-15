ddaae83ca954d41374a8af520ba14e26ab6d4e9d
unknown <usmancali53@gmail.com>
2024-10-15 22:29:12 +0300

    break

---
+++ break.py
@@ -0,0 +1,8 @@
+for num in range(1, 11):
+    if num == 5:
+        continue  # Skip the rest of the code for this iteration when num is 5
+    if num == 8:
+        break     # Exit the loop completely when num is 8
+    print(num)
+
+print("Loop ended")
