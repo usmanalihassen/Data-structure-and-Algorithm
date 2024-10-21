4f2ff3a82bf0bfb12d3eb0b377af0e9ef56d1b46
unknown <usmancali53@gmail.com>
2024-10-21 23:15:46 +0300

    list as queue

---
+++ listasaqueue.py
@@ -0,0 +1,11 @@
+from collections import deque
+queue=deque (["usman", "ali" ,"hassen"])
+queue.append("jemal")
+queue.append("hoden")
+print(queue)
+queue.popleft()
+print(queue)
+queue.popleft()
+print(queue)
+
+            
