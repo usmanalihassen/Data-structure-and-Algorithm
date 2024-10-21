6f6693cd67de941fa5867e623e7462c78c8dcf21
unknown <usmancali53@gmail.com>
2024-10-21 22:54:55 +0300

    list data types

---
+++ listdatatypes.py
@@ -0,0 +1,10 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+print(fruits.count('apple'))
+print(fruits.count('tangerine'))
+print(fruits.index('banana'))
+print(fruits.index('banana', 4))
+print(fruits.reverse())
+print(fruits.append('grape'))
+print(fruits.sort())
+print(fruits.pop())
+print(fruits)
