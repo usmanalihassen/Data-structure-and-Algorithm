beb58b1a2d412860101e16c4a131aae8f66bad94
unknown <usmancali53@gmail.com>
2024-10-25 20:53:49 +0300

    question-1

---
+++ listQ.py
@@ -0,0 +1,10 @@
+#1, How do you create a list with elements 1, 2, 3, 4?
+list=[1,2,3,4,]
+print(list)
+#2, Write code to add an element to the end of a list.
+list.append(2)
+list.pop(2)
+print(list)
+A=[1,2,3,4,5,6,7,8]
+B=A[-3:]
+print(B)
