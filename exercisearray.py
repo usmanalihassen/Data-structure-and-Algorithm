69cabbb1c4ce9503c63910e10a6f647fece01a86
unknown <usmancali53@gmail.com>
2024-10-28 14:26:08 +0300

    maxval

--- exercisearray.py
+++ exercisearray.py
@@ -1,6 +1,14 @@
 array=[7,3,8,2,7,9,1,25]
 minval=array[0]
+
 for i in array:
      if minval > i:
         minval=i
 print(minval)
+#finding maximum value and minimum value 
+array1=[1,3,4,7,54,7,4,3,2,45,53,66,75,33,26,87.4]
+max=array1[0]
+for i in array1:
+    if max < i:
+        max=i
+print(max)
