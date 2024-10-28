4846a58bcd4ddc90934373b8ff46068a65265db6
unknown <usmancali53@gmail.com>
2024-10-28 14:36:29 +0300

    max and min

--- exercisearray.py
+++ exercisearray.py
@@ -6,9 +6,18 @@ for i in array:
         minval=i
 print(minval)
 #finding maximum value and minimum value 
-array1=[1,3,4,7,54,7,4,3,2,45,53,66,75,33,26,87.4]
+array1=[1,3,4,7,54,7,0,4,3,2,45,53,66,75,33,26,87.4]
 max=array1[0]
 for i in array1:
     if max < i:
         max=i
 print(max)
+min=array1[0]
+maxx=array1[0]
+for i in array1:
+    if min > i:
+        min=i
+    elif maxx < i:
+        maxx=i
+print(min)
+print(maxx)
