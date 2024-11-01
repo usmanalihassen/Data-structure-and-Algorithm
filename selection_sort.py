ddd48f012377ee04c0ebc9f0933f289d5d4df752
unknown <usmancali53@gmail.com>
2024-11-01 22:01:24 +0300

    selection-sort

--- selection_sort.py
+++ selection_sort.py
@@ -22,3 +22,11 @@ for i in range(n):
     my_array[i], my_array[min_index] = my_array[min_index], my_array[i]
 
 print("Sorted array:", my_array)
+#EXAMPLE-3
+arr = [5, 2, 4, 6, 1, 3]
+for i in range (len(arr)):
+    min_index=i
+    for j in range (i+1,len(arr)):
+        if arr[j]<arr[min_index]:
+            min_index=arr[j]
+    arr[i],arr[min_index]=arr[min_index],arr[i]
