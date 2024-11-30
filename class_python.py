df8e6461644d6179ca40030b1b0f06b8fc76bb51
unknown <usmancali53@gmail.com>
2024-11-30 12:50:53 +0300

    class in python

--- class_python.py
+++ class_python.py
@@ -4,7 +4,7 @@ class Car:
         self.brand = brand  # Property: brand
         self.color = color  # Property: color
 
-    # Method to describe the car
+    # Method to describe the car/instance method
     def describe(self):
         return f"This is a {self.color} {self.brand}."
 
@@ -25,4 +25,11 @@ class Employee:
         return f"The company is {cls.company}."
 
 print(Employee.get_company())  # Output: The company is TechCorp.
+#static method 
+class Math:
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+print(Math.add(5, 3))  # Output: 8
 
